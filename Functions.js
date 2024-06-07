ae89470651e047c35c15b7d508a7a152ef19d67b
anshul4141 <anshulprajapati691@gmail.com>
2024-06-07 10:55:45 +0530

    done

---
+++ Functions.js
@@ -0,0 +1,32 @@
+function sum(a, b) {
+
+    console.log("Sum = " + (a + b));
+    
+    return a+b;
+
+}
+
+
+var a = 5;
+var b = 10;
+var s = sum(a, b);
+console.log('return = ' + (s));
+
+function login(loginId, password) {
+
+    if (loginId == null && password == null) {
+
+        document.write('<h1 style="color:red">loginId is required</h1>' + '<h1 style="color:red">Password is required</h1>')
+
+    } else if (loginId == 'abc@gmail.com' && password == 'pass123') {
+
+        document.write('<h1 style="color:green">Login successfully</h1>');
+
+    }
+
+}
+
+var loginId = 'abc@gmail.com';
+var password = 'pass123';
+
+login(loginId, password);
