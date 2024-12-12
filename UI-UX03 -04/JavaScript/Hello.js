5b95fdc32a726cc9fd24e2c2d130c0127cd7b8bd
anshul4141 <anshulprajapati691@gmail.com>
2024-12-12 15:51:47 +0530

    done

--- UI-UX03 -04/JavaScript/Hello.js
+++ UI-UX03 -04/JavaScript/Hello.js
@@ -1,8 +1,1 @@
 console.log("Hello World");
-
-let i = 5;
-let j = 10;
-
-let sum = i + j;
-console.log("type of sum = " + typeof(sum));
-console.log(sum);
---
+++ UI-UX03 -04/JavaScript/TestFunction.js
@@ -0,0 +1,26 @@
+// function without argument and without return type
+function greetingToAll() {
+    console.log("Hello To All");
+}
+
+greetingToAll();
+
+// function with argument
+function greet(name) {
+
+    // console.log(`Hello ${name}`);
+    console.log("Hello " + name);
+
+}
+
+greet("Azam")
+
+function sum(a, b) {
+
+    return a + b;
+
+}
+
+let ans = sum(5, 15);
+console.log(sum(5, 15));
+console.log(ans);
